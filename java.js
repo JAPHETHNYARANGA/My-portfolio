3463546ff9f632af68a17a41a175b882411d9fcd
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-07-10 16:28:01 +0300

    fixed sidenav to dissapear on click

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-let closeBtn = document.querySelector('.close');
+let closeBtn = document.querySelector('.right');
 let openBtn = document.querySelector('.fa-bars');
 let sideBarMenu = document.querySelector('.nav');
 // let closeNow = document.querySelector('.close');
@@ -11,6 +11,3 @@ closeBtn.addEventListener('click', ()=>{
     sideBarMenu.style.width="0";
 })
 
-// closeNow.addEventListener('click',()=>{
-//     sideBarMenu.style.width="0";
-// })
